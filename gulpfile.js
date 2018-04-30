328a9dda11570b580b629c1dd18fd702664e68b0
wescley martins <wescleymartins02@gmail.com>
2018-04-30 14:30:14 -0300

    Add gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,25 @@
 var gulp        = require('gulp'),
     sass        = require('gulp-sass'),
+    include     = require('gulp-file-include'),
+    clean       = require('gulp-clean'),
     browserSync = require('browser-sync');
+
+gulp.task('clean', function(){
+    return gulp.src('dist')
+               .pipe(clean());
+})
+
+gulp.task('copy', ['clean'], function(){
+    gulp.src(['src/components/bootstrap/dist/**/*',
+              'src/components/font-awesome/css/**/*',
+              'src/components/font-awesome/fonts/**/*',
+              'src/components/jquery/dist/**/*',
+              'src/css/**/*', 
+              'src/javascript/**/*',
+              'src/imagens/**/*'], {"base": "src"})
+        .pipe(gulp.dest('dist'))
+    
+})
 
 gulp.task('sass', function(){
     gulp.src('./src/sass/**/*.scss')
@@ -8,11 +27,17 @@ gulp.task('sass', function(){
         .pipe(gulp.dest('./src/css/'));
 })
 
+gulp.task('html', function(){
+    return gulp.src('./src/index.html')
+        .pipe(include())
+        .pipe(gulp.dest('./dist/'))
+})
+
 //Server 
-gulp.task('server', function(){
+gulp.task('server', ['html'], function(){
     browserSync.init({
         server:{
-            baseDir: 'src'
+            baseDir: 'dist'
         }
     })
    gulp.watch('./src/**/*').on('change', browserSync.reload)
